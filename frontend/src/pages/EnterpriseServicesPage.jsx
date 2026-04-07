b52353337db9a908f6e01f0c39283a3d3dfde3bd
urvishnu-2002 <i.m.vishnuvardhan2002@gmail.com>
2026-04-07 16:35:47 +0530

    second commit

---
+++ frontend/src/pages/EnterpriseServicesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EnterpriseService from '../components/Services/EnterpriseService';
+
+const EnterpriseServicesPage = () => {
+    return (
+        <main className="pt-0">
+            <title>Enterprise IT Services | HPE IT Solutions</title>
+            <EnterpriseService />
+        </main>
+    );
+};
+
+export default EnterpriseServicesPage;
